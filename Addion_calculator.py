ebc9a5916cd86c0049a4021b9af5b037fcde5d39
anjaliJT <anjali.johnnette@gmail.com>
2023-01-07 16:23:06 +0530

    Added clear button to clear the output

--- Addion_calculator.py
+++ Addion_calculator.py
@@ -14,25 +14,12 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(568, 600)
         MainWindow.setStyleSheet("background-color: rgb(56, 124, 131);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName("gridLayout")
-        self.rslt_bx = QtWidgets.QLineEdit(self.centralwidget)
-        self.rslt_bx.setStyleSheet("font: 75 10pt \"MS Shell Dlg 2\";\n"
-"\n"
-"background-color: rgb(105, 255, 243);")
-        self.rslt_bx.setText("")
-        self.rslt_bx.setObjectName("rslt_bx")
-        self.gridLayout.addWidget(self.rslt_bx, 5, 1, 1, 1)
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setObjectName("label_2")
-        self.gridLayout.addWidget(self.label_2, 3, 0, 1, 1)
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setObjectName("label_3")
-        self.gridLayout.addWidget(self.label_3, 1, 0, 1, 1)
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setMaximumSize(QtCore.QSize(16777215, 100))
         self.label.setStyleSheet("font: 75 12pt \"MS Shell Dlg 2\";\n"
@@ -40,6 +27,29 @@ class Ui_MainWindow(object):
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
         self.gridLayout.addWidget(self.label, 0, 0, 1, 6)
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setObjectName("label_3")
+        self.gridLayout.addWidget(self.label_3, 1, 0, 1, 1)
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setObjectName("label_4")
+        self.gridLayout.addWidget(self.label_4, 6, 4, 1, 1)
+        self.rslt_bx = QtWidgets.QLineEdit(self.centralwidget)
+        self.rslt_bx.setStyleSheet("font: 75 10pt \"MS Shell Dlg 2\";\n"
+"\n"
+"background-color: rgb(255,255,255);")
+        self.rslt_bx.setText("")
+        self.rslt_bx.setObjectName("rslt_bx")
+        self.gridLayout.addWidget(self.rslt_bx, 6, 1, 1, 1)
+        self.Add_bttn = QtWidgets.QPushButton(self.centralwidget)
+        self.Add_bttn.setStyleSheet("\n"
+"background-color: rgb(193, 193, 193);\n"
+"QPUshButton::hover:\n"
+"background-color: rgb(203, 237, 255);")
+        self.Add_bttn.setObjectName("Add_bttn")
+        self.gridLayout.addWidget(self.Add_bttn, 5, 1, 1, 1)
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 3, 0, 1, 1)
         self.Num2_input = QtWidgets.QSpinBox(self.centralwidget)
         self.Num2_input.setStyleSheet("background-color: rgb(105, 255, 243);")
         self.Num2_input.setObjectName("Num2_input")
@@ -48,16 +58,20 @@ class Ui_MainWindow(object):
         self.Num1_input.setStyleSheet("background-color: rgb(105, 255, 243);")
         self.Num1_input.setObjectName("Num1_input")
         self.gridLayout.addWidget(self.Num1_input, 1, 1, 1, 1)
-        self.Add_bttn = QtWidgets.QPushButton(self.centralwidget)
-        self.Add_bttn.setStyleSheet("\n"
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setObjectName("label_5")
+        self.gridLayout.addWidget(self.label_5, 4, 0, 1, 1)
+        self.Num3_input = QtWidgets.QSpinBox(self.centralwidget)
+        self.Num3_input.setStyleSheet("background-color: rgb(105, 255, 243);")
+        self.Num3_input.setObjectName("Num3_input")
+        self.gridLayout.addWidget(self.Num3_input, 4, 1, 1, 1)
+        self.Clr_bttn = QtWidgets.QPushButton(self.centralwidget)
+        self.Clr_bttn.setStyleSheet("\n"
 "background-color: rgb(193, 193, 193);\n"
 "QPUshButton::hover:\n"
 "background-color: rgb(203, 237, 255);")
-        self.Add_bttn.setObjectName("Add_bttn")
-        self.gridLayout.addWidget(self.Add_bttn, 4, 1, 1, 1)
-        self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setObjectName("label_4")
-        self.gridLayout.addWidget(self.label_4, 5, 4, 1, 1)
+        self.Clr_bttn.setObjectName("Clr_bttn")
+        self.gridLayout.addWidget(self.Clr_bttn, 7, 1, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -66,11 +80,13 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label_2.setText(_translate("MainWindow", "Num2"))
-        self.label_3.setText(_translate("MainWindow", "Num1"))
         self.label.setText(_translate("MainWindow", "Addition program"))
-        self.Add_bttn.setText(_translate("MainWindow", "ADD"))
+        self.label_3.setText(_translate("MainWindow", "Num1"))
         self.label_4.setText(_translate("MainWindow", "Result"))
+        self.Add_bttn.setText(_translate("MainWindow", "ADD"))
+        self.label_2.setText(_translate("MainWindow", "Num2"))
+        self.label_5.setText(_translate("MainWindow", "Num3"))
+        self.Clr_bttn.setText(_translate("MainWindow", "CLEAR"))
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -7,19 +7,19 @@ class main_ui(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.Add_bttn.clicked.connect(self.cal_add)
-        self.read_version_file()
+        self.Clr_bttn.clicked.connect(self.clr_rslt)
+
     
     def cal_add(self):
-        self.addition_ = int(self.Num1_input.text()) + int(self.Num2_input.text())
+        self.addition_ = int(self.Num1_input.text()) + int(self.Num2_input.text()) + int(self.Num3_input.text())
         self.rslt_bx.setText(str(self.addition_))
         print(self.addition_)
-        
-    def read_version_file(self):
-        with open("app_version.txt") as file : 
-            data = file.readline()
-            
+    def clr_rslt(self):
+        self.rslt_bx.setText("")
+
             
 if __name__ == '__main__':
+    
     import sys
     app = QtWidgets.QApplication(sys.argv)
     
